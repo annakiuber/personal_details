fa1eaa467ed0870672716b235145adf85ddc517b
anna.kiuber <anna.kiuber@gmail.com>
2018-03-20 13:19:22 -0400

    added sessions

--- app.rb
+++ app.rb
@@ -1,89 +1,69 @@
 require "sinatra"
 
+enable :sessions
+
 get "/" do 
 	erb :first_page
 end
 
 post "/name" do 
-	name = params[:name]
-	redirect "/name_redirect?name="+ name
+	session[:name] = params[:name]
+	redirect "/name_redirect"
 end
-
-
 
 get "/name_redirect" do
-	f_name = params[:name]
-	erb :second_page, locals:{f_name:f_name}
-
-
+	name = session[:name]
+	erb :second_page, locals:{name: name}
 end
+
 post "/age" do
-	f_name = params[:f_name]
-	age = params[:age]
-	redirect "/favorite_food?f_name="+ f_name + "&age=" + age
+	session[:age] = params[:age]
+	redirect "/favorite_food"
 end
 
 get "/favorite_food" do
-	f_name = params[:f_name]
-	age = params[:age]
-	erb :third_page, locals:{f_name:f_name, age:age}
+	name = session[:name]
+	erb :third_page, locals:{name:name}
 end
+
 post "/food_drink" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	redirect "/favorite_numbers?f_name="+ f_name + "&age=" + age + "&food=" + food + "&drink=" + drink
+	session[:food] = params[:food]
+	session[:drink] = params[:drink]
+	redirect "/favorite_numbers"
 end
+
 get "/favorite_numbers" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	erb :forth_page, locals:{f_name:f_name, age:age, food:food, drink:drink}
+	name = session[:name]
+	erb :forth_page, locals:{name:name}
 end
+
 post "/numbers" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	num1 = params[:number_1]
-	num2 = params[:number_2]
-	num3 = params[:number_3]
-	redirect "/favorite_colors?f_name="+ f_name + "&age=" + age + "&food=" + food + "&drink=" + drink + "&num1=" + num1 + "&num2=" + num2 + "&num3=" + num3
+	session[:num1] = params[:number_1]
+	session[:num2] = params[:number_2]
+	session[:num3] = params[:number_3]
+	redirect "/favorite_colors"
 end
+
 get "/favorite_colors" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	num1 = params[:number_1]
-	num2 = params[:number_2]
-	num3 = params[:number_3]
-	erb :fifth_page, locals:{f_name:f_name, age:age, food:food, drink:drink, number_1:num1, number_2:num2, number_3:num3}
+	name = session[:name]
+	erb :fifth_page, locals:{name:name}
 end
+
 post "/colors" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	num1 = params[:number_1]
-	num2 = params[:number_2]
-	num3 = params[:number_3]
-	col1 = params[:color1]
-	col2 = params[:color2]
-	redirect "/results?f_name="+ f_name + "&age=" + age + "&food=" + food + "&drink=" + drink + "&num1=" + num1 + "&num2=" + num2 + "&num3=" + num3 + "&col1=" + col1 + "&col2=" + col2
+	session[:col1] = params[:color1]
+	session[:col2] = params[:color2]
+	redirect "/results"
 end
 
 get "/results" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	num1 = params[:number_1]
-	num2 = params[:number_2]
-	num3 = params[:number_3]
-	col1 = params[:color1]
+	name = session[:name]
+	age = session[:age]
+	food = session[:food]
+	drink = session[:drink]
+	num1 = session[:number_1]
+	num2 = session[:number_2]
+	num3 = session[:number_3]
+	col1 = session[:color1]
 	col2 = params[:color2]
-	erb :sixth_page, locals:{f_name:f_name, age:age, food:food, drink:drink, number_1:num1, number_2:num2, number_3:num3, color1:col1, color2:col2}
+	erb :sixth_page, locals:{name:name, age:age, food:food, drink:drink, number_1:num1, number_2:num2, number_3:num3, color1:col1, color2:col2}
 end
